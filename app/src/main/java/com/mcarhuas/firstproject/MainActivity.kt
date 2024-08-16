fe2a56265bbefda345e021bf8face738e6bfa9bd
Miguel Carhuas <mikel_ct@hotmail.com>
2024-08-15 21:54:17 -0500

    add button code

--- app/src/main/java/com/mcarhuas/firstproject/MainActivity.kt
+++ app/src/main/java/com/mcarhuas/firstproject/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.mcarhuas.firstproject
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var button:Button
+    private lateinit var textView:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        editText = findViewById(R.id.edit_text_id)
+        button = findViewById(R.id.button_id)
+        textView = findViewById(R.id.text_view_id)
+        button.setOnClickListener {
+            val inputText = editText.text.toString()
+            textView.text = editText.text
+        }
+
+
     }
 }
